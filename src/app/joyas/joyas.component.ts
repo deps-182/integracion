e760136b42aab6f4088a6b3c94d9fbbcc4f44e33
deps-182 <s.roa@duocuc.cl>
2024-04-14 23:35:49 -0400

    update 1

---
+++ src/app/joyas/joyas.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-joyas',
+  standalone: true,
+  imports: [],
+  templateUrl: './joyas.component.html',
+  styleUrl: './joyas.component.scss'
+})
+export class JoyasComponent {
+
+}
