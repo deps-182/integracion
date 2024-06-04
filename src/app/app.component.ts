cb5d2142cdbb8f261df3c79571e0a3d37b42e5e6
deps-182 <s.roa@duocuc.cl>
2024-06-04 13:55:27 -0400

    avance

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { RouterLink } from '@angular/router';
+import { CommonModule} from '@angular/common';
+import { RouterLinkActive } from '@angular/router';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './home/home.component.html',
   styleUrl: './app.component.scss'
 })
+
 export class AppComponent {
   title = 'chrono';
 }
